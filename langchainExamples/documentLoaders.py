107442b2f4c18eea0362efe61548940783299713
Juan Peredo <jperedo@bolbeck.com>
2024-02-27 17:59:00 -0600

    Add comments

--- langchainExamples/documentLoaders.py
+++ langchainExamples/documentLoaders.py
@@ -1,12 +1,20 @@
-# import os
-# import openai
-# import sys
+# Import for pdf Loader
 from langchain_community.document_loaders import PyPDFLoader
-# sys.path.append('../..')
 
+# Imports for youTube loader
+from langchain.document_loaders.generic import GenericLoader
+from langchain.document_loaders.parsers import OpenAIWhisperParser
+from langchain.document_loaders.blob_loaders.youtube_audio import YoutubeAudioLoader
+
+# Import for web loader
+from langchain_community.document_loaders import WebBaseLoader
 
 def load_pdf(path):
-
+    """
+    Load a PDF file`
+    :param path: Path to the PDF to be loaded
+    :return: List of document pages
+    """
     loader = PyPDFLoader(path)
     pages = loader.load()   # Each pape in the PDF is a Document object
     print(f'pages in PDF: {len(pages)}')
@@ -15,12 +23,14 @@ def load_pdf(path):
     return pages
 
 
-from langchain.document_loaders.generic import GenericLoader
-from langchain.document_loaders.parsers import OpenAIWhisperParser
-from langchain.document_loaders.blob_loaders.youtube_audio import YoutubeAudioLoader
-
 def youTubeLoader(url, save_dir):
-    # Load a youtube video, pass it to OpenAi's whisper parser to transcribe the video and return the text
+    """
+    Load a YouTube video audio from its URL. Pass the video to OpenAi's whisper parser to transcribe
+     the video and return the text
+    :param url: Url to the YouTube video
+    :param save_dir: Folder to which the video's audio will be saved
+    """
+    #
     loader = GenericLoader(
         YoutubeAudioLoader([url], save_dir),
         OpenAIWhisperParser()
@@ -29,17 +39,23 @@ def youTubeLoader(url, save_dir):
     print(docs[0].page_content[0:500])
 
 
-from langchain_community.document_loaders import WebBaseLoader
+
 def url_loader(url):
-    # This load a web page, but given that it is basically scrapping it, there is a lot of white space
-    #  that could be removed in post processing
+    """
+    This load a web page context. However, given that it is basically scrapping it,
+    there is a lot of white space that could be removed in post-processing
+    :param url: Url to load
+    :return:
+    """
     loader = WebBaseLoader(url)
     docs = loader.load()
     print(f'length of document: {len(docs[0].page_content)}')
     print(f'URL loader first page: \n {docs[0].page_content[:500]}')
 
 def app_run():
-
+    """
+    Loads a PDF, a YouTube video and a web page
+    """
     load_pdf("data/MachineLearning-Lecture01.pdf")
 
     youtube_url_to_cs_course = "https://www.youtube.com/watch?v=jGwO_UgTS7I"
